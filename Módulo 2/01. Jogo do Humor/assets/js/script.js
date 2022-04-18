f866f4ee03bbc7aa865b6fa690cb5cb7137a7cec
SandroXC <sandroxcoelho@gmail.com>
2022-04-18 19:57:46 -0300

    OK

--- Módulo 2/01. Jogo do Humor/assets/js/script.js
+++ Módulo 2/01. Jogo do Humor/assets/js/script.js
@@ -2,6 +2,38 @@ window.onload = function () {
   alert("Olá mundo do JavaScript para Web");
 };
 
+/*const h3 = document.getElementById("position");
+console.dir(h3);
+
+const div_selector = document.querySelector("h3");
+console.log(h3);
+
+elementoButton.addEventListener("click", ( )=> {
+  console.log(Clicaram...)
+}
+);
+*/
+const span = document.querySelector("span");
+const img = document.querySelector("img");
+console.dir(img.attributes);
+
+span.addEventListener("click", () => {
+  console.log("EU FUI CLICADO");
+});
+
+const imgList = ["./assets/images/1jarmila.jpg", "./assets/images/2jurgen.jpg"];
+
+function handleImg() {
+  if (img.attributes.src.nodeValue == imgList[0]) {
+    img.setAttribute("src", "${imgList[1]}");
+    span.innerHTML = "IMAGEM1";
+  } else {
+    img.setAttribute("src", "${imgList[0]}");
+    span.innerHTML = "IMAGEM2";
+  }
+}
+
+/*
 const btnTexto = document.querySelector("#btn-texto");
 
 btnTexto.addEventListener("click", function () {
@@ -22,3 +54,4 @@ titulo.addEventListener("mouseover", function () {
 titulo.addEventListener("mouseout", function () {
   titulo.style.color = "black";
 });
+*/
--- Módulo 2/biah/js/script.js
+++ Módulo 2/biah/js/script.js
@@ -4,29 +4,28 @@ const elementoImagem = document.querySelector("#imagem");
 let elementoButton = document.querySelector("#proximo");
 
 elementoButton.addEventListener("click", () => {
-  if (elementoButton.value == "descontraido") {
-    elementoImagem.src = "./assets/img/irritado.png";
-    elementoTipo.innerText = "Irritado";
+  if (elementoButton.value == "Record 2") {
+    elementoImagem.src = "./assets/img/2jurgen.jpg";
+    elementoTipo.innerText = "Jurgen";
     elementoContexto.innerText =
       "Quando fica irritado o Killua gera momentos engraçados com suas expressões, que são bem marcadas.";
-    elementoButton.value = "irritado";
-  } else if (elementoButton.value == "irritado") {
-    elementoImagem.src = "./assets/img/serio.png";
-    elementoTipo.innerText = "Sério";
+    elementoButton.value = "Record 3";
+  } else if (elementoButton.value == "Record 3") {
+    elementoImagem.src = "./assets/img/3yuriy.jpg";
+    elementoTipo.innerText = "Yuriy";
     elementoContexto.innerText =
       "Quando percebe um perigo ou entra em batalha o Killua fica completamente concentrado. Fruto de anos de treinamento em sua família.";
-    elementoButton.value = "serio";
-  } else if (elementoButton.value == "serio") {
-    elementoImagem.src = "./assets/img/assassino.png";
-    elementoTipo.innerText = "Assassino";
-    elementoContexto.innerText =
-      "Quando o Killua entra no modo assassino os seus olhos perdem completamente o brilho, nesse modo ele é capaz de matar o oponente em segundos.";
-    elementoButton.value = "assassino";
+    elementoButton.value = "Record 3";
+  } else if (elementoButton.value == "Record 3") {
+    elementoImagem.src = "./assets/img/4kostadinova.jpg";
+    elementoTipo.innerText = "Kostadinova";
+    elementoContexto.innerText = "";
+    elementoButton.value = "Record 4";
   } else {
-    elementoImagem.src = "./assets/img/descontraido.png";
-    elementoTipo.innerText = "Descontraído";
+    elementoImagem.src = "./assets/img/1jarmila.jpg";
+    elementoTipo.innerText = "Jarmila";
     elementoContexto.innerText =
       "Quando está com seus amigos, o Killua age como uma criança normal, brincando e se divertindo.";
-    elementoButton.value = "descontraido";
+    elementoButton.value = "Record 1";
   }
 });
