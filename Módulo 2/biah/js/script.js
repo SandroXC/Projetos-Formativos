5b6b3d819e9e0d7152b2fe13293fc9ecc8394d9f
SandroXC <sandroxcoelho@gmail.com>
2022-04-18 20:31:57 -0300

    OK

--- Módulo 2/biah/js/script.js
+++ Módulo 2/biah/js/script.js
@@ -4,28 +4,27 @@ const elementoImagem = document.querySelector("#imagem");
 let elementoButton = document.querySelector("#proximo");
 
 elementoButton.addEventListener("click", () => {
-  if (elementoButton.value == "Record 2") {
+  if (elementoButton.value == "Record2") {
     elementoImagem.src = "./assets/img/2jurgen.jpg";
     elementoTipo.innerText = "Jurgen";
     elementoContexto.innerText =
       "Quando fica irritado o Killua gera momentos engraçados com suas expressões, que são bem marcadas.";
-    elementoButton.value = "Record 3";
-  } else if (elementoButton.value == "Record 3") {
+    elementoButton.value = "Record3";
+  } else if (elementoButton.value == "Record3") {
     elementoImagem.src = "./assets/img/3yuriy.jpg";
     elementoTipo.innerText = "Yuriy";
     elementoContexto.innerText =
       "Quando percebe um perigo ou entra em batalha o Killua fica completamente concentrado. Fruto de anos de treinamento em sua família.";
-    elementoButton.value = "Record 3";
-  } else if (elementoButton.value == "Record 3") {
+    elementoButton.value = "Record4";
+  } else if (elementoButton.value == "Record4") {
     elementoImagem.src = "./assets/img/4kostadinova.jpg";
     elementoTipo.innerText = "Kostadinova";
     elementoContexto.innerText = "";
-    elementoButton.value = "Record 4";
+    elementoButton.value = "Record5";
   } else {
     elementoImagem.src = "./assets/img/1jarmila.jpg";
     elementoTipo.innerText = "Jarmila";
-    elementoContexto.innerText =
-      "Quando está com seus amigos, o Killua age como uma criança normal, brincando e se divertindo.";
-    elementoButton.value = "Record 1";
+    elementoContexto.innerText = "Jarmila 800 metros..";
+    elementoButton.value = "Record1";
   }
 });
