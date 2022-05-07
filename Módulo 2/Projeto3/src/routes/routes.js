12ce4ee128d2280f719553ae1ca2b98e62eb3081
SandroXC <100804321+SandroXC@users.noreply.github.com>
2022-05-07 13:49:41 -0300

    Delete Módulo 2/Projeto3 directory

--- Módulo 2/Projeto3/src/routes/routes.js
+++
@@ -1,6 +0,0 @@
-const routes = require("express").Router();
-const ProjectController = require("../controllers/ProjectController");
-
-routes.get("/", ProjectController.getAll);
-
-module.exports = routes;
