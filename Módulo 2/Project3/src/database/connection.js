972ed7d4489fcf8fd0b259322c8561b515ecbc2b
SandroXC <sandroxcoelho@gmail.com>
2022-05-09 20:44:57 -0300

    Last

--- Módulo 2/Project3/src/database/connection.js
+++ Módulo 2/Project3/src/database/connection.js
@@ -1,18 +1,18 @@
-import Sequelize from 'sequelize'
-import dotenv from 'dotenv'
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const connection = new Sequelize(
-    // process.env.DB_URL
-    'postgres://proj3db_user:ETM5MJ1V0z18pYI0otMQvMN5pZHzt8qU@dpg-c9sq07b0tnujvo4ktdg0-a/proj3db',
-    {
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
-    }
-)
+  process.env.DB_URL,
+  // 'postgres://proj3db_user:ETM5MJ1V0z18pYI0otMQvMN5pZHzt8qU@dpg-c9sq07b0tnujvo4ktdg0-a.oregon-postgres.render.com/proj3db',
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
