5054fea9892d1790c6543ff5320d0434c85096c4
SandroXC <sandroxcoelho@gmail.com>
2022-05-28 20:43:32 -0300

    Last

---
+++ Módulo 3/Projeto 2/API-ElGeladon/src/controllers/paleta.controllerOLD.js
@@ -0,0 +1,92 @@
+const paletasService = require('../services/paleta.service');
+const mongoose = require('mongoose');
+
+const findAllPaletasController = async (req, res) => {
+  const allPaletas = await paletasService.findAllPaletasService();
+
+  if(allPaletas.length == 0) {
+    return res
+      .status(404)
+      .send({ message: 'Não existe nenhuma paleta cadastrada!' });
+  }
+
+  res.send(allPaletas);
+};
+
+const findByIdPaletaController = async (req, res) => {
+  const idParam = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(idParam)) {
+    return res.status(400).send({ message: 'Id inválido!' });
+  }
+
+const chosenPaleta = await paletasService.findByIdPaletaService(parametroId);
+
+  if (!chosenPaleta) {
+    return res.status(400).send({ message: 'Paleta não encontrada!' });
+  }
+
+  res.send(chosenPaleta);
+};
+
+const createPaletaController = async (req, res) => {
+    const paleta = req.body;
+
+     if (
+       !paleta ||
+       !paleta.sabor ||
+       !paleta.descricao ||
+       !paleta.foto ||
+       !paleta.preco
+     ) {
+       return res
+          .status(400)
+          .send({ message: 'Envie todos os campos da paleta!' });
+     }
+
+    const newPaleta = await paletasService.createPaletaService(paleta);
+    res.status(201).send(newPaleta);
+  };
+  
+  const updatePaletaController = async (req, res) => {
+    const idParam = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(idParam)) {
+      return res.status(400).send({ message: 'Id invalido!' });
+    }
+
+    const editPaleta = req.body;
+    
+    if (
+       !editPaleta ||
+       !editPaleta.sabor ||
+       !editPaleta.descricao ||
+       !editPaleta.foto ||
+       !editPaleta.preco
+    ) {
+      return res
+        .status(400)
+        .send({ message: 'Envie todos os campos da paleta!' });
+    }
+    
+    const updatedPaleta = await paletasService.updatePaletaService(idParam, paletaEdit);
+    res.send(updatedPaleta);
+  };
+  
+  const deletePaletaController = async (req, res) => {
+    const idParam = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(idParam)) {
+      return res.status(400).send({ message: 'Id invalido!' });
+    }
+    await paletasService.deletePaletaService(idParam);
+    res.send({ message: 'Paleta deletada com sucesso!' });
+  };
+
+  module.exports = {
+    findAllPaletasController,
+    findByIdPaletaController,
+    createPaletaController,
+    updatePaletaController,
+    deletePaletaController,
+  };
+ 
---
+++ Módulo 3/Projeto 2/ReadMe.md
@@ -0,0 +1,2 @@
+<h3>TO DO LIST - #02</h3>
+Backend CRUD MongoDB + Front Vanilla (Create, Update, Delete)
