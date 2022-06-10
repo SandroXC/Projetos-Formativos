0e1e8be5b3b98a694133d7beff7f2bc7a70ee4ea
SandroXC <sandroxcoelho@gmail.com>
2022-06-09 23:00:09 -0300

    Last

--- Módulo 3/Projeto 4/cardapio-el-geladon/src/components/PaletaListaItem/PaletaListaItem.jsx
+++ Módulo 3/Projeto 4/cardapio-el-geladon/src/components/PaletaListaItem/PaletaListaItem.jsx
@@ -1,40 +1,18 @@
 import "./PaletaListaItem.css";
 
-function PaletaListaItem() {
-  
-    const removerItem = (i) => console.log('remover' + i);
-	const adicionarItem = (i) => console.log('adicionar' + i);
-	const paletaSelecionada = [0];
-	const index = 0;
-	const paleta = {
-		titulo: "Açaí com Leite Condensado",
-		descricao:
-		"Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
-		foto: require("assets/images/acai-com-leite-condensado.png"),
-		preco: 10.0,
-		sabor: "Açaí",
-		recheio: "Leite Condensado",
-		possuiRecheio: true,
-	};    
-  
- const removeButton = (canRender, index) =>
-   Boolean(canRender) && (
-      <button className="Acoes__remover" onClick={() => removerItem(index)}>
-        remover
-      </button>
-    );
+function PaletaListaItem({ paleta, quantidadeSelecionada, index, onRemove, onAdd }) {
 
-  const badgeCounter = (canRender, index) =>
-    Boolean(canRender) && (
-      <span className="PaletaListaItem__badge">
-        {" "}
-        {paletaSelecionada[index]}{" "}
-      </span>
-    );
-
+const badgeCounter = (canRender, index) =>
+  Boolean(canRender) && (
+    <span className="PaletaListaItem__badge"> {quantidadeSelecionada} </span>
+  ); 
+  
+const removeButton = (canRender, index) =>
+	Boolean(canRender) && (<button className="Acoes__remover" onClick={() => onRemove(index)}>remover</button>);
+  
   return (
     <div className="PaletaListaItem">
-      {badgeCounter(paletaSelecionada[index], index)}
+      {badgeCounter(quantidadeSelecionada, index)}
       <div>
         <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
         <div className="PaletaListaItem__preco">
@@ -44,13 +22,10 @@ function PaletaListaItem() {
         <div className="PaletaListaItem__acoes Acoes">
           <button
             className={`Acoes__adicionar ${
-              !paletaSelecionada[index] && "Acoes__adicionar--preencher"
+              !quantidadeSelecionada && "Acoes__adicionar--preencher"
             }`}
-            onClick={() => adicionarItem(index)}
-          >
-            adicionar
-          </button>
-          {removeButton(paletaSelecionada[index], index)}
+            onClick={() => onAdd(index)}>adicionar</button>
+          {removeButton(quantidadeSelecionada, index)}
         </div>
       </div>
       <img
--- Módulo 3/Projeto 4/cardapio-el-geladon/src/components/paletaLista/PaletaLista.jsx
+++ Módulo 3/Projeto 4/cardapio-el-geladon/src/components/paletaLista/PaletaLista.jsx
@@ -1,9 +1,10 @@
-import { paletas } from "mocks/paletas.js";
 import "./PaletaLista.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PaletaListaItem from "components/PaletaListaItem/PaletaListaItem";
+import { PaletaService } from "services/PaletaService";
 
 function PaletaLista() {
+  const [paletas, setPaletas] = useState([]);
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
 
   const adicionarItem = (paletaIndex) => {
@@ -18,12 +19,28 @@ function PaletaLista() {
     setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
   };
 
+  const getLista = async () => {
+    const response = await PaletaService.getLista();
+    setPaletas(response);
+  };
+
+  useEffect(() => {
+    getLista();
+  }, []);
+
   return (
     <div className="PaletaLista">
-      {paletas.map((paleta, index) => (
-        <PaletaListaItem key={`PaletaListaItem-${index}`} />
-      ))}
-    </div>
+	{paletas.map((paleta, index) =>
+		<PaletaListaItem
+			key={`PaletaListaItem-${index}`}
+			paleta={paleta}
+			quantidadeSelecionada={paletaSelecionada[index]}
+			index={index}
+			onAdd={index => adicionarItem(index)}
+			onRemove={index => removerItem(index)} />
+		)
+	}
+</div>
   );
 }
 
